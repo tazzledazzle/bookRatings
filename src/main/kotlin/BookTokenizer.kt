bff6fd51df277e50554453402894a9cf5357e93d
tschumacher <tschumacher@tableau.com>
2017-10-11 12:33:38 -0700

    removed commented lines from BookTokenizer

--- src/main/kotlin/BookTokenizer.kt
+++ src/main/kotlin/BookTokenizer.kt
@@ -8,9 +8,7 @@ class BookTokenizer {
         val re = Regex("[^A-Za-z0-9|\\s+]")
         textFromFile = re.replace(textFromFile, "")
         val tokens = textFromFile.split(" ")
-//        val textLength = tokens.size
         val tokenMap = TreeMap<String, Int>()
-//        println(tokens.size)
         for (token in tokens) {
             if (tokenMap.containsKey(token)) {
                 tokenMap[token] = tokenMap[token]!! + 1
